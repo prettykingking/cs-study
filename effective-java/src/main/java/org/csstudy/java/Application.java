013be877082fa77de687e05607fa4ffef4faaba3
Jie Zheng <prettykingking@live.com>
2023-05-24 16:50:55 +0800

    feat: new practice effective-java

---
+++ effective-java/src/main/java/org/csstudy/java/Application.java
@@ -0,0 +1,14 @@
+package org.csstudy.java;
+
+import org.csstudy.java.concurrency.Synchronization;
+
+
+/**
+ * Effective Java Entry
+ */
+public class Application
+{
+    public static void main(String[] args) throws InterruptedException {
+        Synchronization.start();
+    }
+}
---
+++ effective-java/src/main/java/org/csstudy/java/concurrency/Synchronization.java
@@ -0,0 +1,30 @@
+package org.csstudy.java.concurrency;
+
+import java.util.concurrent.TimeUnit;
+
+public class Synchronization {
+    private static boolean stopRequested;
+
+    public static void start() throws InterruptedException {
+        Thread backThread = new Thread(() -> {
+            int i = 0;
+            while (!stopRequested()) {
+                i++;
+            }
+
+            System.out.println("Stopped");
+        });
+        backThread.start();
+
+        TimeUnit.SECONDS.sleep(2);
+        requestStop();
+    }
+
+    private static synchronized void requestStop() {
+        stopRequested = true;
+    }
+
+    private static synchronized boolean stopRequested() {
+        return stopRequested;
+    }
+}
