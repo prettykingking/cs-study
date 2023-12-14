dbaa7aec6136eec953b4debb1d65c24682dd74c0
Jie Zheng <prettykingking@live.com>
2023-12-14 17:41:35 +0800

    feat: breath-first search in graph

--- dsa-in-python/dsa/ch14/graph.py
+++ dsa-in-python/dsa/ch14/graph.py
@@ -149,3 +149,36 @@ def construct_path(u, v, discovered):
             walk = parent
         path.reverse()                  # reorient path from u to v
     return path
+
+
+def DFS_complete(g):
+    """Perform DFS for entire graph and return forest as a dictionary.
+
+    Result maps each vertex v to the edge that was used to discover it.
+    (Vertices that are roots of a DFS tree are mapped to None.)
+    """
+    forest = {}
+    for u in g.vertices():
+        if u not in forest:
+            forest[u] = None        # u will be the root of a tree
+            DFS(g, u, forest)
+    return forest
+
+
+def BFS(g, s, discovered):
+    """Perform BFS of the undiscovered portion of Graph g starting at Vertex s.
+
+    discovered is a dictionary mapping each vertex to the edge that was used to
+    discover it during the BFS(s should be mapped to None prior to the call).
+    Newly discovered vertices will be added to the dictionary as a result.
+    """
+    level = [s]                             # first leve includes only s
+    while len(level) > 0:
+        next_level = []                     # prepare to gather newly found vertices
+        for u in level:
+            for e in g.incident_edges(u):   # for every outgoing edge from u
+                v = e.opposite(u)
+                if v not in discovered:     # v is an unvisited vertex
+                    discovered[v] = e       # e is tree edge that discover v
+                    next_level.append(v)    # v will be further considered in next pass
+        level = next_level                  # relabel 'next' level to become current
